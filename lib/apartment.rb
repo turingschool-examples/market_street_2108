3594ecd2184bffa2e2e737a9d03f3afb7caf5fe9
Devin Pile <devinpile@gmail.com>
2021-09-08 09:45:23 -0600

    Add method that with pass test for returning the number of bedrooms

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,11 +1,13 @@
 class Apartment
   attr_reader :number,
               :monthly_rent,
-              :bathrooms
+              :bathrooms,
+              :bedrooms
 
   def initialize(unit_info)
     @number = unit_info[:number]
     @monthly_rent = unit_info[:monthly_rent]
     @bathrooms = unit_info[:bathrooms]
+    @bedrooms = unit_info[:bedrooms]
   end
 end
