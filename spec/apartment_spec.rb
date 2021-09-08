687155f37bc31ef027cf1d1be65e831bf8bc9943
Billy Frey <bfrey16@gmail.com>
2021-09-08 09:21:14 -0600

    Test: add apartment_spec test for iteration pattern 1

---
+++ spec/apartment_spec.rb
@@ -0,0 +1,36 @@
+require './lib/renter'
+require './lib/apartment'
+
+RSpec.describe 'Iteration 1' do
+  before :each do
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  it 'can initialize apartment' do
+    expect(@unit1).to be_an_instance_of(Apartment)
+  end
+
+  it 'can check apartment number' do
+    expect(@unit1.number).to eq("A1")
+  end
+
+  it 'can check monthly rent price' do
+    expect(@unit1.monthly_rent).to eq(1200)
+  end
+
+  it 'can check number of bathrooms' do
+    expect(@unit1.bathrooms).to eq(1)
+  end
+
+  it 'can check number of bedrooms' do
+    expect(@unit1.bedrooms).to eq(1)
+  end
+
+  it 'can check who the renter is' do
+    expect(@unit1.renter).to eq(nil)
+  end
+
+  it 'can check if a renter is sucesfully added' do
+    expect(@unit1.renter).to be_an_instance_of(Renter)
+  end
+end
