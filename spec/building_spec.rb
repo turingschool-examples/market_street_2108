e38d9b2c976dc08f5a65ea6e04626ae14a4f8651
Devin Pile <devinpile@gmail.com>
2021-09-08 10:12:55 -0600

    Add test for if building exists

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -4,6 +4,9 @@ require './lib/building'
 
 RSpec.describe Building do
   before(:each) do
-    building = Building.new
+    @building = Building.new
   end
-  
+
+  it 'exists' do
+    expect(@building).to be_an_instance_of Building
+  end
