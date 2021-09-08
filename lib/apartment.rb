a6dd83b2ecd80f52c4dd6d5b1f83e1120b0c5215
Devin Pile <devinpile@gmail.com>
2021-09-08 09:21:59 -0600

    Add changes to pass exist test

---
+++ lib/apartment.rb
@@ -0,0 +1,5 @@
+class Apartment
+
+  def initialize(unit_info)
+  end
+end
--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -3,11 +3,11 @@ require './lib/apartment'
 
 RSpec.describe Apartment do
   before(:each) do
-    unit1   = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    renter1 = Renter.new("Jessie")
+    @unit1   = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
   end
 
   it 'exists' do
-    expect(unit1).to be_an_instance_of Apartment
+    expect(@unit1).to be_an_instance_of Apartment
   end
-end 
+end
