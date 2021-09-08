26cc88bd14a21a839817658b69f1ec6b34c26d14
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-09-08 10:58:27 -0600

    feat: add apartment code

---
+++ lib/apartment.rb
@@ -0,0 +1,13 @@
+require './lib/renter'
+
+class Apartment
+  attr_reader :unit1, :renter
+  def initialize(unit1)
+    @unit    = {}
+    @renter  = ""
+  end
+
+  def add_renter
+    @name
+  end
+end
