0c3b7cb2ebab2e36d8c016ee15b555136687f6d2
Philip Medlin <phmedlin@gmail.com>
2021-09-08 09:58:06 -0600

    Feat:Intialize building

---
+++ lib/building.rb
@@ -0,0 +1,21 @@
+class Building
+  attr_reader :renters,
+              :units
+
+  def initialize(units = [], renters = [])
+    @units = units
+    @renters = renters
+  end
+
+  def add_unit
+    ######
+  end
+
+  def add_renter
+    ######
+  end
+
+  def average_rent
+    ######
+  end
+end
