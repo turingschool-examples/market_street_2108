8066ede9b33660224f51ec1f5a000bc44cae7ba4
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 10:16:54 -0600

    Refactor tests

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -7,6 +7,8 @@ describe Building do
 
   before(:each) do
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   describe '#initialize' do
@@ -21,27 +23,29 @@ describe Building do
 
   describe '#add_unit' do
     it 'adds unit to a units array' do
-      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-      unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-      @building.add_unit(unit1)
-      @building.add_unit(unit2)
+      @building.add_unit(@unit1)
+      @building.add_unit(@unit2)
 
-      expect(@building.units).to eq([unit1,unit2])
+      expect(@building.units).to eq([@unit1, @unit2])
     end
   end
 
   describe '#renters' do
     it 'returns an array of renter in the Building instance object' do
-      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-      unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-      @building.add_unit(unit1)
-      @building.add_unit(unit2)
+      @building.add_unit(@unit1)
+      @building.add_unit(@unit2)
       renter1 = Renter.new("Aurora")
       renter2 = Renter.new("Tim")
-      unit1.add_renter(renter1)
-      unit2.add_renter(renter2)
+      @unit1.add_renter(renter1)
+      @unit2.add_renter(renter2)
 
       expect(@building.renters).to eq(["Aurora", "Tim"])
+    end
+  end 
+
+  describe '#average_rent' do
+    it 'calculates average rent' do
+
     end
   end
 
