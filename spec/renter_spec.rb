2b66dc55f69650ee2d88c48832776bcef889acad
Karan Mehta <karanm645@gmail.com>
2021-09-08 10:53:34 -0500

    "Spacing (minor)"

--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Iteration 1' do
   before :each do
     @jessie = Renter.new("Jessie")
   end
-  
+
   it '1. Renter Instantiation' do
     expect(@jessie).to be_an_instance_of(Renter)
   end
