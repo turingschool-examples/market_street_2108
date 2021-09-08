d6eaaa6d1aa1828507d38f78107404372ff449bd
John Napier <jcee.napier@gmail.com>
2021-09-08 09:52:07 -0600

    Feature iteration 1

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -2,11 +2,17 @@ class Apartment
   attr_reader :number,
               :monthly_rent,
               :bathrooms,
-              :bedrooms
-  def initialize(unit_info)
+              :bedrooms,
+              :renter
+  def initialize(unit_info, renter = nil)
     @number = unit_info[:number]
     @monthly_rent = unit_info[:monthly_rent]
     @bathrooms = unit_info[:bathrooms]
     @bedrooms = unit_info[:bedrooms]
+    @renter = renter
+  end
+
+  def add_renter(renter)
+    @renter = renter 
   end
 end
