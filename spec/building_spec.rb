137f0f7a05b74b8be7bd7d646867a570831793ad
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-09-08 09:25:36 -0600

    Add renters test

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -4,6 +4,9 @@ require "./lib/building"
 describe Class do
   before(:each) do
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @renter1 = Renter.new("Aurora")
   end
   it 'exists' do
     expect(@building).to be_a(Building)
@@ -11,4 +14,21 @@ describe Class do
   it 'has a units array' do
     expect(@building.units).to eq([])
   end
+  it 'adds units' do
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    expect(@building.units).to eq([@unit1, @unit2])
+  end
+  it 'has renters array' do
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    expect(@building.renters).to eq([])
+  end
+  it 'has renters array' do
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @unit1.add_renter(@renter1)
+    expect(@building.renters).to eq([@renter1])
+  end
+
 end
