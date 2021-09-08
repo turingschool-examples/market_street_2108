13c158719ff87cb30c6a799f25bbc28dcbb48654
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:29:47 -0600

    Create #initialize test

---
+++ spec/building_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+
+describe Building do
+
+  describe '#initialize' do
+    it "creates an instance object of Building class"
+      expect(building).to be_an_instance_of(Building)
+    end
+  end
+
+end
