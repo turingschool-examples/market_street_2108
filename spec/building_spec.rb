e4f8de436e7dd51bf1fe86ffcf9c87af1dd2f227
Devin Pile <devinpile@gmail.com>
2021-09-08 10:22:15 -0600

    Add test for adding units to a building

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -5,6 +5,8 @@ require './lib/building'
 RSpec.describe Building do
   before(:each) do
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   it 'exists' do
@@ -13,5 +15,12 @@ RSpec.describe Building do
 
   it 'can return units' do
     expect(@building.units).to eq([])
+  end
+
+  it 'can add units' do
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    expect(building.units).to be_an Array
   end
 end
