b2e662b3ceeaace92b06cd6e288ee69cfc6f505f
John Napier <jcee.napier@gmail.com>
2021-09-08 10:01:51 -0600

    Feature: add apartments to before each, add it has units test

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -6,11 +6,17 @@ RSpec.describe 'Iteration 2' do
   before :each do
     @renter1 = Renter.new("Jessie")
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @building = Building.new
   end
 
   it 'exists' do
 
     expect(@building).to be_an_instance_of(Building)
+  end
+
+  it 'has units' do
+
+    expect(@building.units).to eq([])
   end
 end
