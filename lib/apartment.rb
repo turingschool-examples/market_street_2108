1041fe455fa68b5812f728e1e31e4b1e6ad9e328
Devin Pile <devinpile@gmail.com>
2021-09-08 09:42:21 -0600

    Add method that will pass return number of bathrooms test

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,11 @@
 class Apartment
   attr_reader :number,
-              :monthly_rent
+              :monthly_rent,
+              :bathrooms
 
   def initialize(unit_info)
     @number = unit_info[:number]
     @monthly_rent = unit_info[:monthly_rent]
+    @bathrooms = unit_info[:bathrooms]
   end
 end
