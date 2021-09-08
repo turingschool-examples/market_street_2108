26e3419db71697d68eecc77aad51e066961527f2
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:41:26 -0600

    Create #add_units test

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -5,16 +5,28 @@ require './lib/building'
 
 describe Building do
 
+  before(:each) do
+    @building = Building.new
+  end
+
   describe '#initialize' do
     it "creates an instance object of Building class" do
-      building = Building.new
-      expect(building).to be_an_instance_of(Building)
+      expect(@building).to be_an_instance_of(Building)
     end
 
     it 'has readable attributes' do
-      building = Building.new
+      expect(@building.units).to eq([])
+    end
+  end
 
-      expect(building.units).to eq([])
+  describe '#add_units' do
+    it 'adds units to a units array' do
+      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+      unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+      @building.add_unit(unit1)
+      @building.add_unit(unit2)
+
+      expect(@building.units).to eq([unit1,unit2])
     end
   end
 
