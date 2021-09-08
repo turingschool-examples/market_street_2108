da321611e904b66887f5755a7e5652f3b1288fbe
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:10:19 -0600

    Refactor #initializze test

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -8,7 +8,7 @@ describe Apartment do
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
   end
 
-  desrcibe '#initialize' do
+  describe '#initialize' do
     it 'Creates an instance object of Apartment class' do
       expect(@unit1).to be_an_instance_of(Apartment)
     end
