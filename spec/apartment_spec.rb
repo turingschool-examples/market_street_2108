f3c548ceb69ca1a4d15d2527e33e99b840e89051
Billy Frey <bfrey16@gmail.com>
2021-09-08 09:40:56 -0600

    test: update test to properly run iteration 1 word for word

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -2,35 +2,41 @@ require './lib/renter'
 require './lib/apartment'
 
 RSpec.describe 'Iteration 1' do
-  before :each do
-    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-  end
 
   it 'can initialize apartment' do
-    expect(@unit1).to be_an_instance_of(Apartment)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1).to be_an_instance_of(Apartment)
   end
 
   it 'can check apartment number' do
-    expect(@unit1.number).to eq("A1")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1.number).to eq("A1")
   end
 
   it 'can check monthly rent price' do
-    expect(@unit1.monthly_rent).to eq(1200)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1.monthly_rent).to eq(1200)
   end
 
   it 'can check number of bathrooms' do
-    expect(@unit1.bathrooms).to eq(1)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1.bathrooms).to eq(1)
   end
 
   it 'can check number of bedrooms' do
-    expect(@unit1.bedrooms).to eq(1)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1.bedrooms).to eq(1)
   end
 
   it 'can check who the renter is' do
-    expect(@unit1.renter).to eq(nil)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    expect(unit1.renter).to eq(nil)
   end
 
   it 'can check if a renter is sucesfully added' do
-    expect(@unit1.renter).to be_an_instance_of(Renter)
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Billy")
+    unit1.add_renter(renter1)
+    expect(unit1.renter).to be_an_instance_of(Renter)
   end
 end
