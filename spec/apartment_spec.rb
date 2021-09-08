d2faef4b73077cf241f963f6939e9434f349ca33
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:08:13 -0600

    Create #initialize test

---
+++ spec/apartment_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/renter'
+require './lib/apartment'
+
+describe Apartment do
+
+  before(:each) do
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  desrcibe '#initialize' do
+    it 'Creates an instance object of Apartment class' do
+      expect(@unit1).to be_an_instance_of(Apartment)
+    end
+  end
+
+end
