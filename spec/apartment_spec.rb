9b2f581f1fc6c247265895d10517c2c56c600fe1
Karan Mehta <karanm645@gmail.com>
2021-09-08 10:53:13 -0500

    "Create apartment TDD"

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -10,4 +10,30 @@ RSpec.describe Apartment do
   it "exists" do
     expect(@unit1).to be_an_instance_of(Apartment)
   end
+
+  it "has a unit number" do
+    expect(@unit1.number).to eq("A1")
+  end
+
+  it "returns monthly_rent" do
+    expect(@unit1.monthly_rent).to eq(1200)
+  end
+
+  it "returns the amount of bathrooms" do
+    expect(@unit1.bathrooms).to eq(1)
+  end
+
+  it "returns the amount of bedrooms" do
+    expect(@unit1.bedrooms).to eq(1)
+  end
+
+  it "has nil renters by default" do
+    expect(@unit1.renter).to be nil
+  end
+
+  it "can add renters" do
+    @unit1.add_renter(@renter1)
+    
+    expect(@unit1.renter).to eq(@renter1)
+  end
 end
