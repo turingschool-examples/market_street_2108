cfc443b09902e4c5b9cbe1ee5add0b4247416d51
Billy Frey <bfrey16@gmail.com>
2021-09-08 09:41:37 -0600

    Feat: add apartment class with methods for iteration 1

---
+++ lib/apartment.rb
@@ -0,0 +1,33 @@
+class Apartment
+  attr_reader :unit
+
+  def initialize(unit)
+    @unit = unit
+    @renter = nil
+  end
+
+  def number
+    @unit[:number]
+  end
+
+  def monthly_rent
+    @unit[:monthly_rent]
+  end
+
+  def bathrooms
+    @unit[:bathrooms]
+  end
+
+  def bedrooms
+    @unit[:bedrooms]
+  end
+
+  def renter
+    @renter
+  end
+
+  def add_renter(renter)
+    @renter
+  end
+
+end
