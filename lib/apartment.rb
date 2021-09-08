51d4e173def18e518d9079ef0c2a10bb3a7550ae
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:22:37 -0600

    Create attr_reader

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,6 +1,17 @@
 class Apartment
 
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
   def initialize(apartment_info)
+    @number = apartment_info.fetch(:number)
+    @monthly_rent = apartment_info.fetch(:monthly_rent)
+    @bathrooms = apartment_info.fetch(:bathrooms)
+    @bedrooms = apartment_info.fetch(:bedrooms)
+    @renter = nil
   end
 
 end
