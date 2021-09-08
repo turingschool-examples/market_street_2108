aa0f311e480766e20131d9641f96e96dc9becaf0
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 09:15:10 -0600

    Add readable attributes test

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -12,6 +12,14 @@ describe Apartment do
     it 'Creates an instance object of Apartment class' do
       expect(@unit1).to be_an_instance_of(Apartment)
     end
+
+    it 'has readable attributes' do
+      expect(@unit1.number).to eq("A1")
+      expect(@unit1.monthly_rent).to eq(1200)
+      expect(@unit1.bathrooms).to eq(1)
+      expect(@unit1.bedrooms).to eq(1)
+      expect(@unit1.bedrooms).to be_nil
+    end
   end
 
 end
