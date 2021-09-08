7b9d195ee7bf988a0a994f1e28bbf840ebc00d87
Devin Pile <devinpile@gmail.com>
2021-09-08 09:39:20 -0600

    Add method to pass return monthly rent test

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,7 +1,9 @@
 class Apartment
-  attr_reader :number
+  attr_reader :number,
+              :monthly_rent
 
   def initialize(unit_info)
     @number = unit_info[:number]
+    @monthly_rent = unit_info[:monthly_rent]
   end
 end
