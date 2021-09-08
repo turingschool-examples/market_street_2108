fd15dfb2bb3cbda26fc20e2193da5cfe92b1ded6
Karan Mehta <karanm645@gmail.com>
2021-09-08 10:52:57 -0500

    "Add apartment capabilities"

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,5 +1,19 @@
 class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
   def initialize(apartment_data)
-    # @number = apartment_data[:number]
+    @number = apartment_data[:number]
+    @monthly_rent = apartment_data[:monthly_rent]
+    @bathrooms = apartment_data[:bathrooms]
+    @bedrooms = apartment_data[:bedrooms]
+    @renter = renter
+  end
+
+  def add_renter(renter_addition)
+    @renter = renter_addition
   end
 end
