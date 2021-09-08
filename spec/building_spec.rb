f9f93f90e11d176a5fadae4fe36d4441cb220f55
Billy Frey <bfrey16@gmail.com>
2021-09-08 10:00:25 -0600

    Test: add iteration 2 tests for building class

---
+++ spec/building_spec.rb
@@ -0,0 +1,59 @@
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+
+RSpec.describe 'Iteration 2' do
+
+  it 'can initialize building' do
+    building = Building.new
+    expect(building1).to be_an_instance_of(Building)
+  end
+
+  it 'can store units in an array' do
+    building = Building.new
+    expect(building.units).to eq([])
+  end
+
+  it 'can add units to the building' do
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    expect(building.units).to eq([unit1, unit2])
+  end
+
+  it 'can store the renter of the unit' do
+    building = Building.new
+
+    expect(building.renters).to eq([])
+  end
+
+  it 'can give an array of names of renters' do
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    renter1 = Renter.new("Aurora")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    unit1.add_renter(renter1)
+
+    expect(building.renters).to eq(["Aurora"])
+
+    renter2 = Renter.new("Tim")
+    unit1.add_renter(renter2)
+
+    expect(building.renters).to eq(["Aurora", "Tim"])
+  end
+
+  it 'can give the average rent of the units' do
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    expect(building.average_rent).to eq(1099.5)
+  end
+end
