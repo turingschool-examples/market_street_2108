064f2027bf131c3ebd3d24efff05b2be7c658c91
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 10:28:08 -0600

    Add to before(:each)

--- spec/building_spec.rb
+++ spec/building_spec.rb
@@ -9,6 +9,8 @@ describe Building do
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
   end
 
   describe '#initialize' do
