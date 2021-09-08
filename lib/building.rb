d6930288d9fc6347fdc92d719c0005fa88f8bee3
Philip Medlin <phmedlin@gmail.com>
2021-09-08 10:42:47 -0600

    Iteration 2

--- lib/building.rb
+++ lib/building.rb
@@ -7,15 +7,24 @@ class Building
     @renters = renters
   end
 
-  def add_unit
-    ######
+  def add_unit(unit)
+    units << unit
   end
 
-  def add_renter
-    ######
+  def renters
+    list_of_renters = []
+    units.each do |unit|
+      list_of_renters << unit.renter
+     end
+    list_of_renters
   end
+
 
   def average_rent
-    ######
+    average = []
+    @units.each do |unit|
+      average << unit.monthly_rent
+      end
+    ((average.sum).to_f / (average.count).to_f).round(2)
   end
 end
