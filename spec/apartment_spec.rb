11967ba1dbff4920720328b771d3e749a82851d5
John Napier <jcee.napier@gmail.com>
2021-09-08 09:30:48 -0600

    Feature add renter test and unit number test

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -3,12 +3,22 @@ require './lib/apartment'
 
 RSpec.describe 'Iteration 2' do
   before :each do
-    @jessie = Renter.new("Jessie")
+    @renter1 = Renter.new("Jessie")
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
   end
 
   it 'exists' do
 
     expect(@unit1).to be_an_instance_of(Apartment)
+  end
+
+  it 'has a renter do' do
+
+    expect(@renter1.name).to eq("Jessie")
+  end
+
+  it 'has a unit number' do
+
+    expect(@unit.number).to eq("A1")
   end
 end
