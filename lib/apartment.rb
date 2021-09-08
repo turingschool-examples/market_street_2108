a8ed38b478acc7323785ded81a2c0fc193be4ce9
Sam Devine <samuel.will.devine@gmail.com>
2021-09-08 09:17:26 -0600

    Iteration 1 Complete

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -2,12 +2,18 @@ class Apartment
   attr_reader :number,
               :monthly_rent,
               :bathrooms,
-              :bedrooms
+              :bedrooms,
+              :renter
 
   def initialize(number, monthly_rent, bathrooms, bedrooms)
     @number         = number
     @monthly_rent   = monthly_rent
     @bathrooms      = bathrooms
     @bedrooms       = bedrooms
+    @renter         = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 end
--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -5,27 +5,44 @@ require './lib/apartment.rb'
 RSpec.describe do
   context 'Apartment Instantiation' do
     before :each do
-      @apartment = Apartment.new("A1", 1200, 1, 1)
+      @unit1 = Apartment.new("A1", 1200, 1, 1)
     end
 
     it 'exists' do
-      expect(@apartment).to be_an Apartment
+      expect(@unit1).to be_an Apartment
     end
 
     it '#number' do
-      expect(@apartment.number).to eq "A1"
+      expect(@unit1.number).to eq "A1"
     end
 
     it '#monthly_rent' do
-      expect(@apartment.monthly_rent).to eq 1200
+      expect(@unit1.monthly_rent).to eq 1200
     end
 
     it '#bathrooms' do
-      expect(@apartment.bathrooms).to eq 1
+      expect(@unit1.bathrooms).to eq 1
     end
 
     it '#bedrooms' do
-      expect(@apartment.bedrooms).to eq 1
+      expect(@unit1.bedrooms).to eq 1
+    end
+  end
+
+  context 'Renter Interaction' do
+    before :each do
+      @unit1 = Apartment.new("A1", 1200, 1, 1)
+      @renter1 = Renter.new("Jessie")
+    end
+
+    it 'starts empty' do
+      expect(@unit1.renter).to be_nil
+    end
+
+    it 'can add a renter' do
+      @unit1.add_renter(@renter1)
+
+      expect(@unit1.renter).to eq @renter1
     end
   end
 end
